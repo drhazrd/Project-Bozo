4b29dc3d653b858f302c9a31bec77d1ee9191fb0
ZekiSNL <66867234+ZekiSNL@users.noreply.github.com>
2025-10-14 20:40:20 -0400

    Character Movement + Facing Direction

--- character_medium.gd
+++ character_medium.gd
@@ -1,29 +1,27 @@
 extends CharacterBody3D
 
 @onready var ap: AnimationPlayer = $AnimationPlayer
-
+@onready var camera_3d: Camera3D = $"../Cam Point/Camera3D"
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
+const TURN_SPEED = 8.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	var direction := (camera_3d.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction.length() > 0:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		var target_yaw = atan2(direction.x, direction.z)
+		rotation.y = lerp_angle(rotation.y, target_yaw, TURN_SPEED * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
@@ -34,7 +32,7 @@ func _physics_process(delta: float) -> void:
 func animation_handler():
 	if is_on_floor() and velocity == Vector3.ZERO:
 		ap.play("idle/Root|Idle")
-	if not is_on_floor():
+	elif not is_on_floor():
 		ap.play("jump/Root|Jump")
-	if velocity != Vector3.ZERO and is_on_floor() :
+	elif velocity != Vector3.ZERO and is_on_floor():
 		ap.play("walk/Root|Walk")
--- rotation_point.gd
+++ rotation_point.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@onready var character_large_male: CharacterBody3D = $"../characterLargeMale"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if InputEventMouseMotion
+	position = character_large_male.position
