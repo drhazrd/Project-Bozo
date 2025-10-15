6831875ebb33c3b72a8cbe3895b21abcf1fa4991
Matthew Zolciak <Zolciak.matthew@gmail.com>
2025-10-15 13:52:42 -0400

    Menu Edits
    Created Menu Buttons and created script for menu buttons to work.

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	print("Start Pressed")
+
+
+func _on_options_button_pressed() -> void:
+	print("Options Pressed")
+
+
+func _on_quit_button_pressed() -> void:
+	print("Quit Pressed")
